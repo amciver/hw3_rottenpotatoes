24e1e79999fa746baa38d223304fc96caf2c3cc0
Aaron McIver <aaron.mciver@gmail.com>
2012-10-26 22:54:27 -0700

    Added functionality for HW3-2

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -6,7 +6,6 @@ Given /the following movies exist/ do |movies_table|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -21,9 +20,23 @@ end
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
-
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.gsub!(/[']/, '')
+  rating_list.split(',').each do |rating|
+    if uncheck == "un"
+      step "I uncheck \"ratings_#{rating.strip}\""
+    else
+      step "I check \"ratings_#{rating.strip}\""
+    end
+  end
+end
+
+#Then /I should see all of the movies/ do |movies_table|
+#  movies_table.hashes do |movie|
+#    p movie.inspect
+#  end
+#end
+Then /^I should see all of the movies:$/ do |table|
+  # table is a | The Terminator          | R      | 26-Oct-1984  |pending
+     assert_equal(table.hashes.length, page.all("table#movies tbody/tr").count)
 end
