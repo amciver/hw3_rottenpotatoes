bac382f5502a3bf530e925ec070613784615ba9d
Aaron McIver <aaron.mciver@gmail.com>
2012-10-27 15:16:43 -0700

    Added functionality for HW3-3

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -34,7 +34,20 @@ Scenario: restrict to movies with 'PG' or 'R' ratings
 | Raiders of the Lost Ark | PG     | 12-Jun-1981  |
 
 Scenario: no ratings selected
-  # see assignment
+  When I uncheck the following ratings: 'G', 'PG', 'PG-13', 'R', 'NC-17'
+  And I press "Refresh"
+  Then I should see all of the movies:
+    | title                   | rating | release_date |
+    | Aladdin                 | G      | 25-Nov-1992  |
+    | The Terminator          | R      | 26-Oct-1984  |
+    | When Harry Met Sally    | R      | 21-Jul-1989  |
+    | The Help                | PG-13  | 10-Aug-2011  |
+    | Chocolat                | PG-13  | 5-Jan-2001   |
+    | Amelie                  | R      | 25-Apr-2001  |
+    | 2001: A Space Odyssey   | G      | 6-Apr-1968   |
+    | The Incredibles         | PG     | 5-Nov-2004   |
+    | Raiders of the Lost Ark | PG     | 12-Jun-1981  |
+    | Chicken Run             | G      | 21-Jun-2000  |
 
 Scenario: all ratings selected
   When I check the following ratings: 'G', 'PG', 'PG-13', 'R', 'NC-17'
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,25 +1,18 @@
-# Add a declarative step here for populating the DB with movies.
-
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     Movie.create(movie)
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
   end
 end
 
-# Make sure that one string (regexp) occurs before or after another one
-#   on the same page
-
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  movies = Array.new
+  page.all("table#movies tbody/tr").each do |row|
+      movies += row.all("td[1]").collect.each { |element| element.text  }
+  end
+
+  assert(movies.index(e1) < movies.index(e2), "#{e1} should occur before #{e2}")
 end
 
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   rating_list.gsub!(/[']/, '')
   rating_list.split(',').each do |rating|
@@ -31,12 +24,6 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   end
 end
 
-#Then /I should see all of the movies/ do |movies_table|
-#  movies_table.hashes do |movie|
-#    p movie.inspect
-#  end
-#end
 Then /^I should see all of the movies:$/ do |table|
-  # table is a | The Terminator          | R      | 26-Oct-1984  |pending
-     assert_equal(table.hashes.length, page.all("table#movies tbody/tr").count)
+     assert_equal(table.hashes.length, page.all("table#movies tbody/tr").count, "Movie count is not what it should be")
 end
